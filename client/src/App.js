4691aeffd37b1c9f8701dac1b6f4c2fbf58d8b71
Daniel Patterson <dan24patt@gmail.com>
2019-06-16 21:49:37 -0400

    start ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import "./App.css";
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+
+export function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>Look, Material!</Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
-
-export default App;
