5ab313d23a3c648d46391d1711f73243e8e42e75
Daniel Patterson <dan24patt@gmail.com>
2019-06-28 15:28:04 -0400

    Outline recipe mutations

--- server/src/graphql/recipe.js
+++ server/src/graphql/recipe.js
@@ -1,4 +1,18 @@
 const typeDefs = `
+  input RecipeInput {
+    url: URL!
+  }
+
+  input RecipeParserInput {
+    url: URL!
+    name: ParseInstruction!
+    servings: ParseInstruction!
+    ingredients: [ParseInstruction!]!
+    method: [ParseInstruction!]!
+  }
+
+  scalar ParseInstruction
+
   type Recipe {
     name: String!
     url: URL!
@@ -48,7 +62,21 @@ const typeDefs = `
   }
 
   type Instruction {
+    index: PositiveInt!
     text: String!
+  }
+
+  type RecipeParseResult {
+    recipe: Recipe!
+  }
+
+  type Query {
+    recipes: [Recipe!]!
+  }
+
+  type Mutation {
+    addRecipe(input: RecipeInput!): RecipeParseResult!
+    addRecipeParser(input: RecipeParserInput!): RecipeParseResult!
   }
 `;
 
