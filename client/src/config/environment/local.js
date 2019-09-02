40e4c83dc0105455db82a21bcac67933a47b9fb9
Daniel Patterson <dan24patt@gmail.com>
2019-09-02 17:19:42 -0400

    Enable HTTPS server

--- client/src/config/environment/local.js
+++ client/src/config/environment/local.js
@@ -1,6 +1,6 @@
 export function buildConfig() {
   return {
-    apiURL: "http://localhost:3000/graphql",
+    apiURL: "https://localhost:3000/graphql",
     environment: "LOCAL",
     loggingEnabled: true,
     faviconPaths: ["/favicon-dev-32x32.ico", "/favicon-dev-16x16.ico"]
