2926dad6736f39a4a6f8b812e583fa7c7eb05bec
Daniel Patterson <dan24patt@gmail.com>
2019-09-29 11:27:29 -0400

    Log server requests

--- server/src/graphql/graphqlServer.js
+++ server/src/graphql/graphqlServer.js
@@ -1,15 +1,18 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const { ApolloLogExtension } = require("apollo-log");
 const { mergeResolvers, mergeTypes } = require("merge-graphql-schemas");
 const schemas = [require("./meta"), require("./external"), require("./recipe")];
 
 const typeDefs = mergeTypes(schemas.map(_ => _.typeDefs), { all: true });
 const resolvers = mergeResolvers(schemas.map(_ => _.resolvers));
+const extensions = [() => new ApolloLogExtension({ timestamp: true })];
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  playground: true
+  playground: true,
+  extensions
 });
 
 module.exports = { server };
